b6025bf7d29c891a8c031bf28570bdf9860f8375
Sehar <sehar.sagarwala@gmail.com>
2018-11-02 12:38:27 -0500

    Functional interface with lambda expression

--- src/com/company/FunctionalInterfaces/TestIn7.java
+++ src/com/company/FunctionalInterfaces/TestIn7.java
@@ -4,14 +4,9 @@ interface Interf
 {
     public int squareInt(int n);
 }
-class Demo implements Interf{
-   public int squareInt(int n){
-        return n*n;
-    }
-}
 class Test7{
     public static void main (String [] args){
-        Demo d = new Demo();
-        System.out.println("The Square of 4" + d.squareInt(4));
+        Interf in = n->n*n;
+        System.out.println("The Square of 4 --" + in.squareInt(4));
     }
 }
