3452ee5457b42d1a01bf0a6d47240230d757e414
Sehar <sehar.sagarwala@gmail.com>
2018-11-02 13:15:30 -0500

    Predicate Test

--- src/com/company/FunctionalInterfaces/TestPredicate.java
+++ src/com/company/FunctionalInterfaces/TestPredicate.java
@@ -4,9 +4,13 @@ import java.util.function.Predicate;
 
 public class TestPredicate {
     public static void main (String [] args){
-        Predicate <Integer> p = n -> n>10;
 
+        Predicate <Integer> p = n -> n>10;
         System.out.println("Is input greater than 10 --" + p.test(5));
+
+        Predicate <Integer> even = n -> n%2==0;
+        System.out.println(" 99 Even or Not-- " + even.test(99));
+        System.out.println(" 990 Even or Not-- " + even.test(990));
 
 
     }
